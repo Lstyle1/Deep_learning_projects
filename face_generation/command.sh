d433d7a9fea3f7bf4ab057587360974e074b2e62
Lstyle <lstyle_0@qq.com>
2017-09-03 18:34:15 +0800

    GAN on floyd

---
+++ face_generation/command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/mSTrJT4ZSQEE2wNKVqg76g' --NotebookApp.token='' --NotebookApp.allow_origin='*'
